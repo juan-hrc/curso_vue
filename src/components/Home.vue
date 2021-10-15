4920ab79a90f58f07dc226ac1e497498f0e8aaae
Juan <juan.maulini@gba.gob.ar>
2021-10-15 11:55:22 -0300

    clase 2

---
+++ src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <h3>{{apellido}}</h3>
+        <h1>HOLAAAAAA {{ pepe }}</h1>
+        <HelloWorld2 />
+        <button @click="mostrarNombre()">aceptar</button>
+    </div>
+</template>
+
+<script>
+    import HelloWorld2 from "./HelloWorld2.vue";
+
+    export default {
+        components: { HelloWorld2 },
+        name: "Home",
+        props: {
+            pepe: String,
+        },
+        data:()=>({
+            nombre:"julieta",
+            apellido:"diaz",
+            mail:"jd@jd.jd",
+            users:["pedro","juan","carlos"]
+        }),
+        mounted(){
+            this.apellido="prpr"
+        },
+        methods:{
+            mostrarNombre(){
+                alert(this.nombre);
+            },
+            mostrarApellido(){},
+            mostrarMail(){},
+        },
+        computed:{
+            datos(){return "xxxxxxx"+this.nombre+"xxxxxxx"}
+        },
+        watch:{
+            datos(){
+                
+            }
+        },
+
+    };
+</script>
